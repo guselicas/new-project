93079619b5a91bbd1331849c625c976d99be5631
Phyllis Lavonne <162435340+guselicas@users.noreply.github.com>
2024-03-06 11:47:07 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,21 @@
+if(this.scrollTop != top){
+				this.scrollTop = top;
+				
+				this.renderer.scrollRows(top, topDir);
+				
+				this.dispatch("scroll-vertical", top, topDir);
+				this.dispatchExternal("scrollVertical", top, topDir);
+			}
+
+if(typeof subject == "object"){
+			if(subject instanceof Row){
+				//subject is row element
+				return subject;
+			}else if(subject instanceof RowComponent){
+				//subject is public row component
+				return subject._getSelf() || false;
+			}else if(typeof HTMLElement !== "undefined" && subject instanceof HTMLElement){
+				//subject is a HTML element of the row
+				let match = this.rows.find((row) => {
+					return row.getElement() === subject;
+				});
